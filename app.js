de88f4172f048a23451c1060530126166bd3a144
Simon Frezard <simon.frezard@gmail.com>
2021-03-17 15:02:43 +0100

    added expressjs for localhost

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = 3000;
 const URI = "mongodb+srv://dbSimon:dbSimonFrezard@cluster0.wqbnd.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 mongoose.connect(URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -18,8 +29,12 @@ myModel.find(function (err, myModel) {
   console.log(myModel);
 }).countDocuments();
 
-const listing = myModel.find({}, { name: 1 }).limit(5);
+const listing = myModel.find({ name: /a/ }, { name: 1, listing_url: 1 }).skip(10).limit(10).sort({ _id: 1 });
 
-listing.exec((err, result) => {
-  console.log(result);
+app.get('/api', (req, res) => {
+
+  listing.exec((err, result) => {
+    console.log(result);
+    res.send(result);
+  });
 });
